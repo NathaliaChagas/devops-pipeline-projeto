fa98be4ea6bf65b540eddbb26f9b4952ffa381a9
NathaliaChagas <164352178+NathaliaChagas@users.noreply.github.com>
2025-11-23 20:06:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+const express = require('express');
+const app = express();
+
 function saudacao() {
   return "Olá, DevOps! O pipeline está funcionando corretamente.";
 }
 
-console.log(saudacao());
+app.get('/', (req, res) => {
+  res.send(saudacao());
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 module.exports = saudacao;
